a69fae11d4d54966d9d85ad42919f8fb7d245543
Tsering Lama <tseringlama@pursuit.org>
2020-04-27 13:17:41 -0400

    starting strings

---
+++ Strings/strings.m
@@ -0,0 +1,16 @@
+//
+//  strings.m
+//  ObjC-Intro
+//
+//  Created by Tsering Lama on 4/27/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int strings (int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+    }
+    return 0;
+}
